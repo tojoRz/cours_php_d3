d3156cf4c6f5021392921e2c757ad43c3ac3d512
tojoRz <110741604+tojoRz@users.noreply.github.com>
2022-09-17 06:56:54 +0200

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    <section>
+        <h1>Formulaire</h1>
+        <form action="#" method="POST">
+            <input type="text" name="nom">
+            <input type="text" name="prenom">
+            <input type="text" name="age">
+            <input type="text" name="dateN">
+            <input type="submit" value="valider">
+        </form>
+    </section>
+</body>
+
+
+</html>
